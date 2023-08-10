67ff40d3016887f48f8eecd8557d9012a95787da
Aziz-BenLamine <azizbenlamine@gmail.com>
2023-08-10 11:30:22 +0100

    Add getSize() function

--- script.js
+++ script.js
@@ -8,10 +8,33 @@ function createBoard(size){
 
     for(let i = 0; i < numDivs; i++){
         let div = document.createElement('div');
+        div.addEventListener('mouseover', function(){
+            div.style.backgroundColor = "black";
+        })
         board.insertAdjacentElement("beforeend", div);
     }
 };
 
+function getSize(){
+    let input = prompt("Please select a size for the board: ");
+    let message = document.querySelector("#message");
+    if(input == "" || isNaN(input)){
+        message.innerHTML = "Please provide a number";
+    }
+    else if(input < 0 || input > 100){
+        message.innerHTML = "Please provide a number between 1 and 100"
+    }
+    else{
+        message.innerHTML = "Enjoy drawing!"
+        return input;
+    }
+};
+
 document.addEventListener("DOMContentLoaded", function(){
-    createBoard(32);
+    createBoard(16);
+    let selectButton = document.querySelector(".select-button");
+    selectButton.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size);
+    })
 })
