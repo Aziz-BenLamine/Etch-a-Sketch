e59f8c588011b7821b9e0c77e2ca2422eb74a0b8
Aziz-BenLamine <azizbenlamine@gmail.com>
2023-08-10 11:57:09 +0100

    Add color buttons and reset button scripts

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//Default drawing color
+let color = "black";
+
 function createBoard(size){
     let board = document.querySelector('.board');
 
@@ -8,9 +11,7 @@ function createBoard(size){
 
     for(let i = 0; i < numDivs; i++){
         let div = document.createElement('div');
-        div.addEventListener('mouseover', function(){
-            div.style.backgroundColor = "black";
-        })
+        div.addEventListener('mouseover', colorDiv)
         board.insertAdjacentElement("beforeend", div);
     }
 };
@@ -29,6 +30,26 @@ function getSize(){
         return input;
     }
 };
+
+function colorDiv(){
+    if(color == "random"){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+    else{
+        this.style.backgroundColor = 'black';
+    }
+}
+
+function setColor(colorChoice){
+    color = colorChoice;
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll('div');
+    divs.forEach((div) => {
+        div.style.backgroundColor = "white";
+    });
+}
 
 document.addEventListener("DOMContentLoaded", function(){
     createBoard(16);
